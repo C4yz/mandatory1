c47856705b5eecb8f49f1cda640fd05cec0d5aeb
Thomas Hohnen <55696172+C4yz@users.noreply.github.com>
2021-03-03 15:32:26 +0100

    Add files via upload

---
+++ mandatory1.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+echo "Do you want source code or from dpkg/rpm? (enter: source, dpkg, aur og rpm)"
+read installtype
+echo "What package do you want to install?"
+read package
+echo "Enter install link"
+read URL
+
+if [ "$(stat -c '%a' /usr/local/src)" == "777" ]
+then
+  echo "you have permission"
+else
+  echo "you dont have permission"
+  # get permissions to folder read, write and execute
+  sudo chmod 777 /usr/local/src
+
+fi
+
+# Downloads package to folder
+wget -O /usr/local/src/$package $URL
+
+echo $installtype
+if [ $installtype == "source" ]
+then
+    echo "Install type = Source"
+    tar -xzvf $package
+    makepkg -p /usr/local/src/$package
+else
+    echo "Install type does not eaqual source"
+    # Install the package
+    $installtype -i /usr/local/src/$package
+
+fi
